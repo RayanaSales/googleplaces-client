6b7f18563a3855500b2a7bb75fee6133626572c9
Rayana <rayanaasales@gmail.com>
2020-12-28 14:23:40 -0300

    brining back server.js

---
+++ dist/_nuxt/99abcc7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{250:function(t,e,n){"use strict";n.r(e);var r={},l=n(8),component=Object(l.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("Lugares favoritados")])}),[],!1,null,null,null);e.default=component.exports}}]);
