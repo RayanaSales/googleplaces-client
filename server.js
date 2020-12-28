472bbb867711cd0b3c970576263ebe93cad9f4b7
Rayana <rayanaasales@gmail.com>
2020-12-28 20:16:32 -0300

    fix: files for heroku with google maps

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const serveStatic = require('serve-static')
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.listen(port)
